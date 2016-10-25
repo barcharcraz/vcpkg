de84620c1314bf770070d4093cae9fe8b9b0b586
Charles Barto <bartoc@umich.edu>
2016-10-25 13:59:30 -0400

    more qt stuff

--- ports/double-conversion/portfile.cmake
+++ ports/double-conversion/portfile.cmake
@@ -15,31 +15,17 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 1406dc22b4ea71e1a2490f96cfed3230e122b97607c83ba106df4e90c7e4bfdcfc136c88741e7f1127237b38b4944d462ec5a4627a71f5ea3fe14afbcc64cd44
 )
 vcpkg_extract_source_archive(${ARCHIVE})
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/mscv_vers.patch
-)
-
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/msvc/double-conversion.vcxproj
-)
-
-message(STATUS "Installing")
-file(INSTALL
-    ${SOURCE_PATH}/msvc/Debug/Win32/double-conversion.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
-file(INSTALL
-    ${SOURCE_PATH}/msvc/Release/Win32/double-conversion.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-
-file(COPY ${SOURCE_PATH}/double-conversion DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    vcpkg_copy_pdbs()
-endif()
+vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=True)
+vcpkg_install_cmake()
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
+file(RENAME ${CURRENT_PACKAGES_DIR}/CMake ${CURRENT_PACKAGES_DIR}/share/double-conversion)
+file(READ ${CURRENT_PACKAGES_DIR}/debug/CMake/double-conversionLibraryDepends-debug.cmake DEBUG_MODULE)
+string(REPLACE "\${_IMPORT_PREFIX}" "\${_IMPORT_PREFIX}/debug" DEBUG_MODULE "${DEBUG_MODULE}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/double-conversion/double-conversionLibraryDepends-debug.cmake "${DEBUG_MODULE}")
+#file(COPY ${SOURCE_PATH}/double-conversion DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/CMake)
+vcpkg_copy_pdbs()
 
 message(STATUS "Installing done")
 
--- ports/qt5/portfile.cmake
+++ ports/qt5/portfile.cmake
@@ -8,7 +8,7 @@
 
 include(${CMAKE_TRIPLET_FILE})
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/qt-everywhere-opensource-src-5.7.0)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/qt)
 set(JOM_PATH ${CURRENT_BUILDTREES_DIR}/src)
 vcpkg_download_distfile(ARCHIVE
     URLS "http://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz"
@@ -22,23 +22,52 @@ vcpkg_download_distfile(JOM_ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 vcpkg_extract_source_archive(${JOM_ARCHIVE})
+# we need to make the path shorter
+if(EXISTS ${CURRENT_BUILDTREES_DIR}/src/qt-qt-everywhere-opensource-src-5.7.0)
+    file(RENAME ${CURRENT_BUILDTREES_DIR}/src/qt-everywhere-opensource-src-5.7.0 ${CURRENT_BUILDTREES_DIR}/src/qt)
+endif()
+vcpkg_find_acquire_program(PERL)
+#vcpkg_find_acquire_program(PYTHON3)
+file(DOWNLOAD "http://www.orbitals.com/programs/py.exe" "${CURRENT_BUILDTREES_DIR}/src/python.exe"
+    EXPECTED_HASH SHA512=28286bf6c510a4596c49e3100b7c45e5ccceac70404fdda436704b12874ec267617614002675f51b9f02c8b509cacee9aae60fc0c1b3e921755b0d47490a0744)
+#vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH} PATCHES 
+#    ${CMAKE_CURRENT_LIST_DIR}/000_patch_libpng.patch
+#    ${CMAKE_CURRENT_LIST_DIR}/000_patch_libjpeg.patch
+#)
 set(ENV{LIB} "$ENV{LIB};${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}/lib")
 set(ENV{INCLUDE} "$ENV{INCLUDE};${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}/include")
+# need our dependencies for running moc and rcc during the build process
+#get_filename_component(PYTHON3 ${PYTHON3} DIRECTORY)
+get_filename_component(PERL ${PERL} DIRECTORY)
+message(STATUS ${PYTHON3})
+message(STATUS ${PERL})
+set(ENV{PATH} "$ENV{PATH};${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}/bin;${PERL};${CURRENT_BUILDTREES_DIR}/src")
+file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
+file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
+message(STATUS "configuring debug")
+
 vcpkg_execute_required_process(COMMAND ${SOURCE_PATH}/configure.bat 
-    -opensource 
-    -confirm-license 
-    -nomake tests 
+    -opensource
+    -confirm-license
+    -nomake tests
+    -nomake examples
     -system-zlib 
     -system-libjpeg 
     -system-libpng 
     -system-freetype 
-    -system-pcre 
+    -system-pcre
     -system-harfbuzz 
     -system-sqlite
+    -system-doubleconversion
+    -opengl dynamic
+    -debug
     -prefix ${CURRENT_PACKAGES_DIR}
-    ZLIB_LIB="-lzlib"
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
+    ZLIB_LIBS="zlib.lib"
+    WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
 
+message(STATUS "building debug")
+vcpkg_execute_required_process(COMMAND ${JOM_PATH}/jom.exe
+    WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
 
 # Handle copyright
 #file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/qt5)

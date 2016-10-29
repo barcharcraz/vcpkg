d95ff7608293121f253e8f01c6fd7fe8933781d7
Charles Barto <bartoc@umich.edu>
2016-10-29 16:51:35 -0400

    forgot some formating

--- ports/qt5/portfile.cmake
+++ ports/qt5/portfile.cmake
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Common Ambient Variables:
 #   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
 #   TARGET_TRIPLET is the current triplet (x86-windows, etc)
@@ -15,11 +14,6 @@ vcpkg_download_distfile(ARCHIVE
     URLS "http://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz"
     FILENAME "qt-5.7.0.tar.gz"
     SHA512 bde74a474da192785ff8db5565db03685411b577bf29e999f72b92e4f6d406a6dfd266b612c9fe56a996f717ab32811e9396ada3c0d5ce5bc2c3d137b6533174
-)
-vcpkg_download_distfile(JOM_ARCHIVE
-    URLS "http://download.qt.io/official_releases/jom/jom_1_1_1.zip"
-    FILENAME "jom-1.1.0.zip"
-    SHA512 23a26dc7e29979bec5dcd3bfcabf76397b93ace64f5d46f2254d6420158bac5eff1c1a8454e3427e7a2fe2c233c5f2cffc87b376772399e12e40b51be2c065f4
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 vcpkg_extract_source_archive(${JOM_ARCHIVE})
